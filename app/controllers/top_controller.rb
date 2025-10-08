d3180f317efcf5e2b3792dc2c92e90802f2a63cc
Mineyu <2311140072d@ed.fuk.kindai.ac.jp>
2025-10-08 08:49:36 +0000

    KISO 2 finished

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -8,11 +8,19 @@ class TopController < ApplicationController
   end
   
   def login
-    if params[:uid] == "kindai" and params[:pass] == "sanriko"
+    if User.find_by(uid: params[:uid], pass: params[:pass]) != nil
+      logger.debug "ログイン成功"
       session[:login_uid] = params[:uid]
       redirect_to top_main_path
     else
       render "error", status: 442
     end
   end
+  
+  def logout
+    session.delete(:login_uid)
+    redirect_to root_path
+  end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get 'top/main'
   post 'top/login'
   root 'top#main'
+  get 'top/logout'
   
   get "up" => "rails/health#show", as: :rails_health_check
 end
