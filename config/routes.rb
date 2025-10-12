77c493d27a73b0cd1faf0e7d539fbdf65dc99b6e
Mineyu <2311140072d@ed.fuk.kindai.ac.jp>
2025-10-12 14:12:43 +0000

    push to render.com

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,8 @@ Rails.application.routes.draw do
   root 'top#main'
   get 'top/logout'
   
+  get 'top/new', to: 'top#new', as: 'top_new'
+  post 'top/create', to: 'top#create', as: 'top_create'
+  
   get "up" => "rails/health#show", as: :rails_health_check
 end
