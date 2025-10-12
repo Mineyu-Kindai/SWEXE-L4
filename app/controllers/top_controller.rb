9f50bf4e99748721375ff35caac3c912b61105cb
Mineyu <2311140072d@ed.fuk.kindai.ac.jp>
2025-10-12 13:44:46 +0000

    HATTEN finished

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,4 +1,20 @@
 class TopController < ApplicationController
+  
+  def new
+    @user = User.new
+  end
+  
+  def create
+    @user = User.new(uid: params[:user][:uid])
+    @user.pass = BCrypt::Password.create(params[:user][:pass])
+    
+    if @user.save
+      redirect_to top_main_path
+    else
+      render "new"
+    end
+  end
+  
   def main
     if session[:login_uid] != nil
       render "main"
@@ -8,8 +24,11 @@ class TopController < ApplicationController
   end
   
   def login
-    if User.find_by(uid: params[:uid], pass: params[:pass]) != nil
-      logger.debug "ログイン成功"
+    
+    user = User.find_by(uid: params[:uid])
+    
+    if user != nil and BCrypt::Password.new(user.pass) == params[:pass]
+      p "logged in!"
       session[:login_uid] = params[:uid]
       redirect_to top_main_path
     else
